096f34f5fec95ec3f2573dcad52fc6590ea78167
kl3sshydra <81070560+kl3sshydra@users.noreply.github.com>
2022-02-13 19:11:04 +0100

    Add files via upload

---
+++ DISKSPY/DISK_SPY.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import os, shutil
+saving_dir = "SAVED"
+interesting_extensions = ["txt", "ldb", "pdf", "odt", "log", "zip"]
+
+if os.path.exists(saving_dir) == False:
+	os.mkdir(saving_dir)
+
+def isInteresting(f):
+	flag = False
+	for e in interesting_extensions:
+		if (f.endswith(e)):
+			flag = True
+				
+	return flag
+
+
+
+os.system("clear && lsblk")
+disco = input("-----------------------\nSelezionare un mountpoint: ")
+if os.path.exists(disco) == False:
+	print("Il percorso non esiste.")
+	exit()
+print("Sto recuperando files interessanti...")
+for (root, dirs, files) in os.walk(disco, topdown=True):
+	for f in files:
+		if isInteresting(f):
+			print(f)
+			print(root)
+			try:
+				os.makedirs(saving_dir+root)
+			except:
+				pass
+			try:
+				shutil.copyfile(root+"/"+f, saving_dir+root+"/"+f)
+			except:
+				pass
